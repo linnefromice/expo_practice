bd4fe077d27aa74eea88d94beef4d6c2e5426d8d
linnefromice <linnefromice@gmail.com>
2020-01-28 23:09:36 +0900

    modify design

--- expo_counter/App.js
+++ expo_counter/App.js
@@ -1,31 +1,64 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, Divider } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <View style={styles.container}>
-      <Text>{count}</Text>
+    <SafeAreaView style={styles.container}>
       <View>
-        <Button
-          title='PLUS'
-          onPress={() => setCount(count+1)}
-        />
-        <Button
-          title='MINUS'
-          onPress={() => setCount(count-1)}
-        />
+        <View style={styles.msgArea}>
+          <Text>You have pushed the button this many times:</Text>
+        </View>
+        <View style={styles.countDisplayArea}>
+          <Text style={styles.textCount}>{count}</Text>
+        </View>
+        <View style={styles.buttonsArea}>
+          <View style={styles.buttonArea}> 
+            <Button
+              title='PLUS / +'
+              onPress={() => setCount(count+1)}
+            />
+          </View>
+          <View style={styles.buttonArea}> 
+            <Button
+              title='MINUS / -'
+              onPress={() => setCount(count-1)}
+            />
+          </View>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  msgArea: {
+    flex: 1,
+    padding: '0.5rem'
+  },
+  countDisplayArea: {
+    flex: 1,
+    padding: '0.5rem',
+    alignItems: 'center'
+  },
+  buttonsArea: {
+    flex: 1,
+    padding: '0.5rem',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  textCount: {
+    fontSize: 30
+  },
+  buttonArea: {
+    flex: 1,
+    padding: '0.5rem',
+  }
 });
