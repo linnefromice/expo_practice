62200ebe260b390285dabda04253f7478c8e2e20
linnefromice <linnefromice@gmail.com>
2020-02-13 21:49:14 +0900

    add Todo.js using native-base (sample view)

---
+++ expo_todo_native-base/screen/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Text } from 'native-base';
+
+const Todo = () => {
+  return (
+    <Container>
+      <Text>TODO APP</Text>
+    </Container>
+  );
+}
+
+export default Todo;
