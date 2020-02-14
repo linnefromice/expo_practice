750cbe0c99006170cdba217824124e25c9ac687e
linnefromice <linnefromice@gmail.com>
2020-02-14 10:11:38 +0900

    add todo data & display list (only display / dummy logic)

--- expo_todo_react-native-paper/screen/Todo.js
+++ expo_todo_react-native-paper/screen/Todo.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import { Text } from 'react-native-paper'
+import { View } from 'react-native'
+import { List } from 'react-native-paper'
+
+let masterTodoList = [
+  { id: 1, content: "test content 1", isDone: false },
+  { id: 2, content: "test content 2", isDone: true },
+  { id: 3, content: "test content 3", isDone: false }
+]
 
 const Todo = () => {
   return (
-    <Text>DUMMY CONTENT</Text>
+    <View>
+      <List.Section>
+        <List.Subheader>TODO LIST</List.Subheader>
+        <List.Item
+          title={masterTodoList[0].content}
+        />
+        <List.Item
+          title={masterTodoList[1].content}
+          />
+        <List.Item
+          title={masterTodoList[2].content}
+          />
+      </List.Section>
+    </View>
   );
 }
 
