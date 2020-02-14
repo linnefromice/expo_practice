3f850fc65b2fe01a4a5baf16c608c9073fd0858d
linnefromice <linnefromice@gmail.com>
2020-02-14 19:22:58 +0900

    add recentId in store for MobX

--- expo_todo_native-base/store/todoStore.js
+++ expo_todo_native-base/store/todoStore.js
@@ -1,13 +1,19 @@
 import { observable, computed, action, decorate, autorun } from 'mobx';
 
 class TodoStore {
+  recentId = 3;
   todoList = [
     { id: 1, content: "test content 1 for MobX", isDone: false },
     { id: 2, content: "test content 2 for MobX", isDone: true },
     { id: 3, content: "test content 3 for MobX", isDone: false }
   ];
 
+  get getRecentId(){ return recentId; }
   get getTodoList(){ return todoList; }
+
+  incrementRecentId = () => {
+    this.recentId += 1;
+  }
 
   addNewTodo = todo => {
     this.todoList.push(todo);
@@ -30,11 +36,12 @@ class TodoStore {
       }
     }
   }
-  
 }
 
 decorate(TodoStore, {
+  recentId: observable,
   todoList: observable,
+  getRecentId: computed,
   getTodoList: computed,
   addNewTodo: action,
   removeTodo: action,
@@ -46,5 +53,6 @@ let store = new TodoStore();
 export default store;
 
 autorun (() => {
+  console.log(store.recentId);
   console.log(store.todoList);
 });
