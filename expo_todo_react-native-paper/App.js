e4704427e6e3d48bf2a3f1ffbb06da62ae6db476
linnefromice <linnefromice@gmail.com>
2020-02-14 10:04:12 +0900

    remove unnecessary declaration

--- expo_todo_react-native-paper/App.js
+++ expo_todo_react-native-paper/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Appbar } from 'react-native-paper';
 import Todo from './screen/Todo';
 
